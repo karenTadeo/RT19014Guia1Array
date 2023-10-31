22e63d63a3358238ce318f8d35c89632567a7054
karenTadeo <143974606+karenTadeo@users.noreply.github.com>
2023-10-31 00:27:41 -0600

    first commit

---
+++ src/main/java/com/mycompany/rt1901guia1array/RT1901Guia1Array.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rt1901guia1array;
+
+/**
+ *
+ * @author Tadeo
+ */
+public class RT1901Guia1Array {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
